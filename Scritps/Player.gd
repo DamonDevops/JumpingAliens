a62a85dca60c3701e3d24a42bc6543b657846a56
Damon Jacquemin <damon.jacquemin@gmail.com>
2023-12-26 19:38:15 +0100

    Scoring system

--- Scritps/Player.gd
+++ Scritps/Player.gd
@@ -5,6 +5,7 @@ extends CharacterBody2D
 @export var gravity :float = 500.0
 
 var score :int = 0
+@onready var score_text :Label = get_node("CanvasLayer/ScoreText")
 
 func _physics_process(delta):
 	if not is_on_floor():
@@ -29,3 +30,4 @@ func game_over():
 	
 func add_score(amount):
 	score += amount
+	score_text.text = str("Score: ", score)
